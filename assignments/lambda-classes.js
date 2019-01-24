5c2764d832aed9027f5a8b40f5ac2bcfc3a06f02
Maxime Salomon <maxime@croissant.io>
2019-01-24 15:54:14 +0100

    MVP done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,125 @@
 // CODE here for your Lambda Classes
+
+
+// #### Person
+
+// * First we need a Person class. This will be our `base-class`
+// * Person receives `name` `age` `location` `gender` all as props
+// * Person receives `speak` as a method.
+// * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
+
+class Person {
+    constructor({name, location, gender}) {
+        this.name = name;
+        this.location = location;
+        this.gender = gender;
+    }
+    speak () {
+        return `Hello my name is ${this.name} I am from ${this.location}`;
+    }
+};
+
+
+// #### Instructor
+
+// * Now that we have a Person as our base class, we'll build our Instructor class.
+// * Instructor uses the same attributes that have been set up by Person
+// * Instructor has the following unique props:
+//   * `specialty` what the Instructor is good at i.e. 'redux'
+//   * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
+//   * `catchPhrase` i.e. `Don't forget the homies`
+// * Instructor has the following methods:
+//   * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+//   * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+
+
+class Instructor extends Person {
+    constructor({name, location, gender, specialty, favLanguage, catchPhrase}) {
+        super({name, location, gender});
+        this.specialty = specialty;
+        this.favLanguage = favLanguage;
+        this.catchPhrase = catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+};
+
+// #### Student
+
+// * Now we need some students!
+// * Student uses the same attributes that have been set up by Person
+// * Student has the following unique props:
+//   * `previousBackground` i.e. what the Student used to do before Lambda School
+//   * `className` i.e. CS132
+//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+// * Student has the following methods:
+//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
+//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+
+
+class Student extends Person {
+    constructor({name, location, gender, previousBackground, className, favSubjects}) {
+        super({name, location, gender});
+        this.previousBackground = previousBackground;
+        this.className = className;
+        this.favSubjects = favSubjects;
+    }
+    listsSubjects() {
+        // `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+        this.favSubjects.forEach(function(e) {
+            return `${e}`
+        });
+    }
+    PRAssignement(subject) {
+        return `${student.name} has submitted a PR for ${subject}`
+    }
+    sprintChallenge(subject) {
+        return `${student.name} has begun sprint challenge on ${subject}`
+    }
+};
+
+// #### Project Manager
+
+// * Now that we have instructors and students, we'd be nowhere without our PM's
+// * ProjectManagers are extensions of Instructors
+// * ProjectManagers have the following uniqe props:
+//   * `gradClassName`: i.e. CS1
+//   * `favInstructor`: i.e. Sean
+// * ProjectManangers have the following Methods:
+//   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
+//   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
+
+
+class ProjectManager extends Instructor {
+    constructor({name, location, gender, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}) {
+        super({name, location, gender, specialty, favLanguage, catchPhrase});
+        this.gradClassName = gradClassName;
+        this.favInstructor = favInstructor;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!​`;
+    };
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    };
+};
+
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`,
+  });
+
+  console.log(fred);
+
+
